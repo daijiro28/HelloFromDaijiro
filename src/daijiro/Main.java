25403295f8e7076357150a3d0519162d6d4d6d35
YabeAbe <abe19001@byui.edu>
2020-10-10 20:19:38 +0900

    Greeting from Yuki

--- src/daijiro/Main.java
+++ src/daijiro/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Daijiro");
+
+        System.out.println("Hello Daijiro, my name is Yuki.");
     }
 }
